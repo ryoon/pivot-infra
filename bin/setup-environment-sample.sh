ad4bf5c1cef2580e9a31d29c342653b7d7c11fe0
smartini <smartini@13f79535-47bb-0310-9956-ffa450edef68>
2012-12-31 16:39:47 +0000

    updated release env scripts to latest JDK versions

--- bin/setup-environment-sample.sh
+++ bin/setup-environment-sample.sh
@@ -24,8 +24,8 @@ export TEMP_DIR=/tmp
 export OPT=/opt
 
 export ANT_HOME=$OPT/apache-ant-1.8.4
-export JAVA_HOME=$OPT/jdk1.6.0_37
-# export JAVA_HOME=$OPT/jdk1.7.0_09
+export JAVA_HOME=$OPT/jdk1.6.0_38
+# export JAVA_HOME=$OPT/jdk1.7.0_10
 export MAVEN_HOME=$OPT/apache-maven-2.2.1
 # export MAVEN_HOME=$OPT/apache-maven-3.0.4
 
